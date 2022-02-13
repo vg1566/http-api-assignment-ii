214d35fdcd9e3f7e7cfd1a2f152c0bf2fe09c3b6
VGZ <vg1566@rit.edu>
2022-02-13 12:16:20 -0500

    GET and HEAD both working

---
+++ src/jsonResponses.js
@@ -0,0 +1,52 @@
+let users = {};
+
+// responds with json obj (GET)
+const respondJSON = (request, response, status, object) => {
+  const headers = {
+    'Content-Type': 'application/json',
+  };
+
+  response.writeHead(status, headers);
+  response.write(JSON.stringify(object));
+  response.end();
+};
+
+// responds without json obj (HEAD)
+const respondJSONMeta = (request, response, status) => {
+  const headers = {
+    'Content-Type': 'application/json',
+  };
+
+  response.writeHead(status, headers);
+  response.end();
+};
+
+const getUsers = (request, response) => {
+  let content = users;
+  const responseJSON = {
+    content,
+  };
+
+  return respondJSON(request, response, 200, responseJSON);
+};
+
+const getUsersMeta = (request, response) => respondJSONMeta(request, response, 200);
+
+const notFound = (request, response) => {
+  const responseJSON = {
+    message: 'The page you are looking for was not found.',
+    id: 'notFound',
+  };
+  return respondJSON(request, response, 404, responseJSON);
+};
+
+const notFoundMeta = (request, response) => {
+  return respondJSONMeta(request, response, 404);
+};
+
+module.exports = {
+  getUsers,
+  getUsersMeta,
+  notFound,
+  notFoundMeta,
+};
---
+++ src/server.js
@@ -0,0 +1,44 @@
+const http = require('http'); // http module
+const url = require('url'); // url module
+const query = require('querystring');
+const htmlHandler = require('./htmlResponses.js');
+const jsonHandler = require('./jsonResponses.js');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+// object to route requests to proper handlers
+const urlStruct = {
+  GET: {
+    '/': htmlHandler.getIndex,
+    '/style.css': htmlHandler.getCSS,
+    '/getUsers': jsonHandler.getUsers,
+    notFound: jsonHandler.notFound,
+  },
+  HEAD: {
+    '/getUsers': jsonHandler.getUsersMeta,
+    notFound: jsonHandler.notFound,
+  },
+};
+
+const onRequest = (request, response) => {
+  // parse info from url
+  const parsedUrl = url.parse(request.url);
+
+  // grab params
+  const params = query.parse(parsedUrl.query);
+
+  // grab accepted types
+  const acceptedTypes = request.headers.accept.split(',');
+
+  // handle the request. If no handler found, use notFound
+  if (urlStruct[request.method][parsedUrl.pathname]) {
+    urlStruct[request.method][parsedUrl.pathname](request, response, acceptedTypes, params);
+  } else {
+    urlStruct[request.method].notFound(request, response, acceptedTypes);
+  }
+};
+
+// start server
+http.createServer(onRequest).listen(port, () => {
+  console.log(`Listening on 127.0.0.1: ${port}`);
+});
